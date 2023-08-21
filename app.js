fce15c5e86b3047114a389f2e26ab379adaa59aa
brayan-coy <brayan324554@gmail.com>
2023-08-21 14:02:04 -0500

    first commit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //BOTON DE REDIRECCION DEL ADMIN
 document.getElementById("adminButton").addEventListener("click", function () {
-  window.location.href = "/Validacion/index.html"; // Cambia "nuevo_index.html" con la URL de tu nuevo index
+  window.location.href = "/validacion.html"; // Cambia "nuevo_index.html" con la URL de tu nuevo index
 });
 
 //SECCION DE LA BARRA DE DESPLIEGUE DE PRODUCTOS
---
+++ script.js
@@ -0,0 +1,39 @@
+var timeoutID;
+
+function redirigirAIndex() {
+  window.location.href = "index.html"; // Redirigir al index.html
+}
+
+function iniciarTimeout() {
+  clearTimeout(timeoutID); // Reiniciar el timeout
+  timeoutID = setTimeout(redirigirAIndex, 1800000); // Configurar timeout para redirigir después de 30 minutos (1800000 milisegundos)
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  document
+    .getElementById("ingresarButton")
+    .addEventListener("click", function () {
+      iniciarTimeout(); // Reiniciar el timeout al hacer clic en el botón
+      pedirDatos();
+    });
+});
+
+function pedirDatos() {
+  var usuarioSeleccionado = document.getElementById("usuarioSelector").value;
+  var contrasenaIngresada = document.getElementById("contrasena").value;
+
+  // Verifica las credenciales según el usuario seleccionado
+  if (usuarioSeleccionado === "martha" && contrasenaIngresada === "23") {
+    window.location.href = "otrapagina.html";
+  } else if (usuarioSeleccionado === "pepe" && contrasenaIngresada === "456") {
+    window.location.href = "otrapagina.html";
+  } else if (usuarioSeleccionado === "jorge" && contrasenaIngresada === "789") {
+    window.location.href = "otrapagina.html";
+  } else {
+    alert("Datos incorrectos. Inténtalo nuevamente.");
+    iniciarTimeout(); // Reiniciar el timeout después de un intento fallido
+  }
+}
+
+// Iniciar el timeout cuando se carga la página
+iniciarTimeout();
